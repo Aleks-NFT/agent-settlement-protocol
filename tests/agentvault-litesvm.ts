451801ae37308a749dbab892897a2827dd700705
Aleks_ <aleks@nft.kyiv.ua>
2026-04-30 11:05:36 +0000

    fix(test): use single LiteSVM client (before not beforeEach) to prevent C++ heap OOM

--- tests/agentvault-litesvm.ts
+++ tests/agentvault-litesvm.ts
@@ -18,13 +18,12 @@ describe("agentvault [litesvm]", () => {
   let provider: LiteSVMProvider;
   let program: anchor.Program<any>;
 
-  beforeEach(() => {
+  before(() => {
     client = fromWorkspace(".");
     provider = new LiteSVMProvider(client);
     program = new anchor.Program(IDL, provider);
   });
 
-  afterEach(() => { if ((global as any).gc) (global as any).gc(); });
   function airdrop(pk, sol = 100) {
     provider.client.airdrop(pk, BigInt(sol * 1_000_000_000));
   }
